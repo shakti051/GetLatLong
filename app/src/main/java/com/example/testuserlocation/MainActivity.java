b2d5cc1dfb700139ec11a7bad9fc60404ce08f7d
dell <shaktithemastermind@gmail.com>
2020-07-14 21:54:13 +0530

    Getting Current Lattitude and Longitude with Physical address

--- app/src/main/java/com/example/testuserlocation/MainActivity.java
+++ app/src/main/java/com/example/testuserlocation/MainActivity.java
@@ -5,15 +5,23 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient client;
@@ -41,6 +49,51 @@ public class MainActivity extends AppCompatActivity {
                         if (location!=null)
                             textView1.setText("Lattitude: "+ location.getLatitude()+"\n"+
                                  "Longitude"+   location.getLongitude());
+                        /***Using Geocoder getting the physical address of Lat Long**/
+                        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                        try {
+                            List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            if (listAddresses != null && listAddresses.size() > 0) {
+
+                                Log.i("PlaceInfo", listAddresses.get(0).toString());
+
+                                String address = "";
+
+                                if (listAddresses.get(0).getSubThoroughfare() != null) {
+
+                                    address += listAddresses.get(0).getSubThoroughfare() + " ";
+
+                                }
+
+                                if (listAddresses.get(0).getThoroughfare() != null) {
+
+                                    address += listAddresses.get(0).getThoroughfare() + ", ";
+
+                                }
+
+                                if (listAddresses.get(0).getLocality() != null) {
+
+                                    address += listAddresses.get(0).getLocality() + ", ";
+
+                                }
+
+                                if (listAddresses.get(0).getPostalCode() != null) {
+
+                                    address += listAddresses.get(0).getPostalCode() + ", ";
+
+                                }
+
+                                if (listAddresses.get(0).getCountryName() != null) {
+
+                                    address += listAddresses.get(0).getCountryName();
+
+                                }
+
+                                Toast.makeText(getApplicationContext(), address, Toast.LENGTH_SHORT).show();
+                               }
+                            } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
             }
